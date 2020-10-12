e5b87765cc4614ffdd17406397dad9a2dccd8318
coopzone-dc <70201105+coopzone-dc@users.noreply.github.com>
2020-10-12 16:29:00 +0100

    Add files via upload

--- RunCPM/hardware/esp32.h
+++ RunCPM/hardware/esp32.h
@@ -2,12 +2,13 @@
 #define ESP32_H
 
 SdFat SD;
-#define SDINIT 14,2,15,13 // TTGO_T1,LOLIN32_Pro
-//#define SDINIT 18,19,23,5 // ePaper,ESP32_DevKit
+//#define SDINIT 14,2,12,13 // TTGO_T1,LOLIN32_Pro
+//#define SDINIT 14,16,17,13 // TTGO_T1,LOLIN32_Pro
+//#define SDINIT 18=clk,19=miso,23=mosi,5=cs // ePaper,ESP32_DevKit
 #define SDMHZ 25 // TTGO_T1,LOLIN32_Pro=25 ePaper,ESP32_DevKit=20
-#define LED 5 // TTGO_T1=22 LOLIN32_Pro=5(inverted) DOIT_Esp32=2 ESP32-PICO-KIT=no led
+#define LED 34 // TTGO_T1=22 LOLIN32_Pro=5(inverted) DOIT_Esp32=2 ESP32-PICO-KIT=no led
 #define LEDinv 1
-#define BOARD "LOLIN32_Pro (clone)"
+#define BOARD "VGA32 (clone)"
 #define board_esp32
 #define board_digital_io
 
